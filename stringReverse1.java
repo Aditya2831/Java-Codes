a2cb0a02fa68167fcdbf5b485bc499e93c6c5395
Aditya2831 <adinaman31@gmail.com>
2023-04-24 10:46:09 +0530

    .

---
+++ stringReverse1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class stringReverse1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String str2="";
+
+        for(int i=str.length()-1;i>=0;i--)
+        {
+           str2=str2+str.charAt(i);
+        }
+
+        System.out.println(str2);
+    }
+}
